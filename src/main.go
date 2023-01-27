635ef91c8dd8a313b7776a8bc985285848961f74
Derek Paul <11dereksamuel@gmail.com>
2023-01-27 17:22:39 -0500

    car.go deleted DON'T TO PROD

--- src/main.go
+++ src/main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	c "github.com/dereksamuel/gocourse/src/car"
-	"github.com/google/uuid"
-)
-
 func main() {
-	var otherCar = c.CarPublic{Label: "sadsadsdsd", Owner: c.Person{Name: "Jorge", Lastname: "Paul"}}
-
-	fmt.Printf("Hello, world %s\n", "👁️")
-	fmt.Println(uuid.New().String())
-	fmt.Println(otherCar)
 }
