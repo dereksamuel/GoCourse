d971506086ad8726735f735a098d51cb032d22b5
Derek Paul <11dereksamuel@gmail.com>
2023-01-30 15:55:09 -0500

    done

--- src/main.go
+++ src/main.go
@@ -2,13 +2,41 @@ package main
 
 import "fmt"
 
-func getArea(resultArea chan int, base int) {
-	resultArea <- base * base
+func msgMaker(text string, channel chan string) {
+	channel <- text
 }
 
 func main() {
-	resultArea := make(chan int, 1)
-	go getArea(resultArea, 5)
+	channel := make(chan string, 2)
 
-	fmt.Println(<-resultArea)
+	channel <- "Hola"
+	// channel <- "Mundo"
+
+	fmt.Println(len(channel), cap(channel)) // length and capacity
+
+	// para cerrar el canal cuando no lo estes usando mas
+	close(channel)
+
+	// TODO: run this with an error:
+	// channel <- "Mundo3"
+
+	for message := range channel {
+		fmt.Println(message)
+	}
+
+	// Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go msgMaker("Msg 1", email1)
+	go msgMaker("Msg 2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case ml1 := <-email1:
+			fmt.Println("Send email", ml1)
+		case ml2 := <-email2:
+			fmt.Println("Send email", ml2)
+		}
+	}
 }
