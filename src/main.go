609cc3ee50c837e01768b358fd6e1b90fefc4f77
Derek Paul <11dereksamuel@gmail.com>
2023-01-30 12:36:55 -0500

    done

--- src/main.go
+++ src/main.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/dereksamuel/gocourse/src/pc"
 )
 
+type Pc struct {
+	Ram       int
+	Disk_rang int
+	Brand     string
+}
+
+// un ping nos va a retornar un mensaje a detalle
+func (myPC Pc) PingMethod(anotherMsg string) {
+	fmt.Println("-*-*-*-*-*-*-*-*-* ping -*-*-*-*-*-*-*-*-*")
+	fmt.Println(anotherMsg)
+	fmt.Println(myPC.Brand, "Pong")
+	fmt.Println("-*-*-*-*-*-*-*-*-* ping end -*-*-*-*-*-*-*-*-*")
+}
+
+func (myPC *Pc) DuplicateRam() {
+	myPC.Ram = myPC.Ram * 2
+}
+
+// *pc: acceder a values del valor en memoria de myPC
+
+func (myPCArg Pc) String() string {
+	return fmt.Sprintf("Ram: %d, DiskRange %d, Brand %s", myPCArg.Ram, myPCArg.Disk_rang, myPCArg.Brand) // va a crear un string
+}
+
 func main() {
-	a := 50
-	b := &a // va a ser la direccion en memoria donde se guarda a
+	myPc := Pc{Ram: 15, Brand: "msi", Disk_rang: 100}
 
-	fmt.Println(b)
-	fmt.Println(*b) // el valor al que le apunta la direccion en memoria
-
-	*b = 780
-	fmt.Println(a) // a nos va a dar 780 porque esta apuntando a la misma direccion en memoria de b
-
-	myPc := pc.Pc{Ram: 16, Disk_rang: 82.5, Brand: "MSI"}
-	myPc.PingMethod("Hello new msg")
-
-	fmt.Println(myPc.Ram)
-	myPc.DuplicateRam()
-	fmt.Println(myPc.Ram)
-
-	myPc.DuplicateRam()
-	fmt.Println(myPc.Ram)
+	fmt.Println(myPc.String())
+	fmt.Println(myPc)
 }
