e859da989ffa290d3ae6b714e03dbced456593db
Derek Paul <11dereksamuel@gmail.com>
2023-01-30 15:45:50 -0500

    ok undersand

--- src/main.go
+++ src/main.go
@@ -2,15 +2,13 @@ package main
 
 import "fmt"
 
-func say(text string, c chan<- string) { // solo de entrada
-	c <- text // se le manda el text atributte COM UN PIPELINE
+func getArea(resultArea chan int, base int) {
+	resultArea <- base * base
 }
 
 func main() {
-	channel := make(chan string, 1) // tipo del dato del canal, y buena practica es definirle cantidad limite
+	resultArea := make(chan int, 1)
+	go getArea(resultArea, 5)
 
-	go say("Bye", channel)
-	fmt.Println("Hello")
-	fmt.Println(<-channel)
-	<-channel // ESTO BLOQUEA LA FUNC main
+	fmt.Println(<-resultArea)
 }
